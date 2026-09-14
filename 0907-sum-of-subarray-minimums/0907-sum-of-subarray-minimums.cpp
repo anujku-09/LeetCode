80343b5dd8637a8512da1c65e264f8f7d2574c04
anujku-09 <anujkushwah9876@gmail.com>
2026-09-14 11:24:09 +0530

    Time: 49 ms (91.76%), Space: 177.9 MB (96.69%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -4,38 +4,23 @@ public:
         int n = arr.size();
         long long ans = 0;
         long long mod = 1e9 + 7;
-        vector<int>res1(n);
-        vector<int>res2(n);
-        stack<int> st1, st2;
-        for(int i = 0 ; i < n; i++){
-            while(!st1.empty() && arr[st1.top()] > arr[i]){
-                st1.pop();
+        stack<int> st;
+        for(int i = 0 ; i <= n; i++){
+            int curr = (i == n ? 0 : arr[i]);
+            while(!st.empty() && arr[st.top()] > curr){
+                int mid = st.top();
+                st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int right = i;
+                long long l = mid - left;
+                long long r = right - mid;
+                long long prod = arr[mid] * l * r;
+
+                ans = (ans + prod) % mod;
             }
-            if(st1.empty()){
-                res1[i] = -1;
+            if(i < n){
+                st.push(i);
             }
-            else{
-                res1[i]= st1.top();
-            }
-            st1.push(i);
-        }
-        for(int i = n - 1; i >= 0; i--){
-            while(!st2.empty() && arr[st2.top()] >= arr[i]){
-                st2.pop();
-            }
-            if(st2.empty()){
-                res2[i]= n;
-            }
-            else{
-                res2[i] = st2.top();
-            }
-            st2.push(i);
-        }
-        for(int i = 0; i < n; i++){
-            long long left = i - res1[i];
-            long long right = res2[i] - i;
-            long long total_subarrays = (left * right) % mod;
-            ans = (ans + (long long)arr[i] * total_subarrays) % mod;
         }
         return ans;
     }
