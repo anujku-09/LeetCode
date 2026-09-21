fb7ec5108b83b83c88b8a2b3f6d26f24de216f06
anujku-09 <anujkushwah9876@gmail.com>
2026-09-21 18:18:37 +0530

    Time: 3 ms (62.65%), Space: 18.5 MB (66.1%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    struct cmp{
+        bool operator()(ListNode* a, ListNode* b){
+            return a->val > b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+        for(ListNode* head : lists){
+            if(head != nullptr){
+                pq.push(head);
+            }
+        }
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while(!pq.empty()){
+            ListNode* node = pq.top();
+            pq.pop();
+            tail->next = node;
+            tail = node;
+            if(node->next != nullptr)
+                pq.push(node->next);
+        }
+        return dummy.next;
+    }
+};
